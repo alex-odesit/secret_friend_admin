fd23730851a71b56098789b8ae1a0f71eb78730b
Alex Dmitreiv <a.dmitriev@gamzix.com>
2023-11-21 11:44:08 +0200

    clear

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,43 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    root: true,
+    env: {
+        node: true
+    },
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: [
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        parser: '@typescript-eslint/parser',
+        ecmaFeatures: {
+            jsx: false
+        }
+    },
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'linebreak-style': 'off',
+        'vue/multi-word-component-names': 'off',
+        quotes: ['warn', 'single'],
+        'no-tabs': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '--resolveJsonModule': 'off',
+        'prettier/prettier': [
+            'off',
+            {
+                endOfLine: 'auto'
+            }
+        ],
+        'no-debugger': 'off',
+        'no-undef': 'off'
+    }
 };
