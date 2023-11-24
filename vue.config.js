1a41d4ad346aed84773295e99c2d8a6625358c12
Alex Dmitreiv <a.dmitriev@gamzix.com>
2023-11-24 18:18:05 +0200

    fix errors

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        devServer: {
+            headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*' },
+            compress: true,
+            disableHostCheck: true
+        }
+    },
+    publicPath: './',
+    outputDir: 'dist',
+    assetsDir: 'static'
 });
